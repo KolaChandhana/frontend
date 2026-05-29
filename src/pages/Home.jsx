df98f6f43af737726f48d6e9a32504181360c2fe
KolaChandhana <kolachandhana@gmail.com>
2026-05-29 15:24:33 +0530

    ,

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../api";
+import API from "../Api";
 import Navbar from "../components/Navbar";
 import RecipeCard from "../components/RecipeCard";
 import SearchBar from "../components/SearchBar";
