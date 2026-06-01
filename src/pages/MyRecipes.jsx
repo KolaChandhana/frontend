258aaddadebcc6e5d1947beb7ee5634dd683ee9c
KolaChandhana <kolachandhana@gmail.com>
2026-06-01 11:22:50 +0530

    frontend

--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../api";
+import API from "../Api";
 import "./MyRecipes.css";
 const MyRecipes = () => {
     const navigate = useNavigate();
