5895c333d8df663fb4e1777e51e1d899c1da745d
KolaChandhana <kolachandhana@gmail.com>
2026-06-04 16:35:39 +0530

    frontend

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://recipi-backend-z2f8.onrender.com/api"
 });
 
 export default API;
